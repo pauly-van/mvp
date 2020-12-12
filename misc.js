cb7d742b154a80f2655f70de3e0c48ad8339d165
Paul Van <paulyv8n@gmail.com>
2020-12-12 00:16:32 -0800

    correct db schema and adding

---
+++ misc.js
@@ -0,0 +1,4 @@
+steamData = {
+    apiKey: '0271B60737867C0743F3B762B0B52ECB',
+    steamUrl: `https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=440&count=3`
+}
--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const port = 3000;
 const path = require('path');
 const app = express();
-const port = 3000;
+const axios  = require('axios');
+const bodyParser = require('body-parser');
+const steamData = require('../config.js');
+const db = require('../database');
 
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res)=>{
-  res.send(index).status(200);
+app.get('/refresh', (req, res)=>{
+  db.retrieveFromMongo((game)=>{
+    console.log(game);
+  })
 });
 
 app.post('/', (req, res)=>{
-  res.send('got post request');
+  let id = Object.keys(req.body);
+  axios({
+    method: 'GET',
+    url: `${steamData.steamUrl}?appid=${id[0]}&count=1`,
+  })
+  .then((res)=>{
+    db.saveToMongo(res);
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
 });
 
 app.listen(port, ()=>console.log('Server is listening on port '+port));
