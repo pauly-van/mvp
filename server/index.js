3e0d9f73c7d5b420bad6b2f0f138d0ba71becabc
Paul Van <paulyv8n@gmail.com>
2020-12-12 08:00:20 -0800

    MVP candidate

--- server/index.js
+++ server/index.js
@@ -11,16 +11,45 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/refresh', (req, res)=>{
+  let gameIDs = [];
   db.retrieveFromMongo((game)=>{
-    console.log(game);
+    game.forEach(g=>{
+      gameIDs.push(g.appId);
+    })
+    for(let i = 0; i<gameIDs.length;i++){
+      axios({
+        method: 'GET',
+        url: `${steamData.steamUrl}?appid=${gameIDs[i]}&count=1`
+      })
+      .then((res)=>{
+        //delete database
+        db.saveToMongo(res);
+      })
+      .catch((err)=>{
+        if(err){console.log(err)};
+      })
+    }
+    db.retrieveFromMongo((newGame)=>{
+      res.status(200).send(newGame);
+    })
   })
 });
+
+app.get('/gameList', (req, res)=>{
+  axios({
+    method: 'GET',
+    url: 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/'
+  })
+  .then((data)=>{
+    res.send(data.data.applist.apps);
+  })
+})
 
 app.post('/', (req, res)=>{
   let id = Object.keys(req.body);
   axios({
     method: 'GET',
-    url: `${steamData.steamUrl}?appid=${id[0]}&count=1`,
+    url: `${steamData.steamUrl}?appid=${id[0]}&count=1`
   })
   .then((res)=>{
     db.saveToMongo(res);
@@ -28,6 +57,7 @@ app.post('/', (req, res)=>{
   .catch((err)=>{
     console.log(err)
   })
+  res.status(200).end();
 });
 
 app.listen(port, ()=>console.log('Server is listening on port '+port));
